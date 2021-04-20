04b3cac0c02fabd7c777293cdd48b569a5f8a46a
Emiliano Rivarola <emi_rivarola@hotmail.com>
2021-04-20 16:38:57 -0300

    add the GET / posts route with its respective controller that shows the detail of the post

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,9 +2,28 @@ const db = require('../database/models')
 
 module.exports = {
     list:(req,res)=>{
-        db.Posts.findAll()
+        db.Posts.findAll({
+            attributes: ['id','title','image','category','date']
+        })
         .then(data=>{
             res.send(data)
+        })
+    },
+    detail:(req,res)=>{
+        db.Posts.findOne({
+            where:{
+                id:req.params.id
+            }
+        })
+        .then(data=>{
+            if (data) {
+                res.send(data)
+            }else{
+                res.send({msg:'The post you are looking for does not exist'})
+            }
+        })
+        .catch(error=>{
+            res.send(error)
         })
     }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ let controllers = require('../controllers/posts');
 
 //list of posts
 router.get('/', controllers.list);
-//
+//detail of post
+router.get('/:id', controllers.detail);
 
 module.exports = router;
